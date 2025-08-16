e082f4ca4ba3b78220cc3d7097eaf01e11de4df5
Shubham <shubham.tandale.dev@gamil.com>
2025-08-17 03:13:51 +0530

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Shubham on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
